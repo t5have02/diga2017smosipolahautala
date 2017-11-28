972077a0de4a381b2e30c39c831b563fabafaa4a
Tuomas Sipola <t5sija02@students.oamk.fi>
2017-11-28 20:35:13 +0200

    Trying to do something!!

---
+++ src/components/ChartContainer.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react'
+import '../css/ChartContainer.css';
+
+class ChartContainer extends Component {
+    render () {
+        return (
+            <div className="Chart">
+               
+            </div>
+        )
+    }
+}
+
+export default ChartContainer
---
+++ src/components/ScenarioHeading.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+class componentName extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Skenaarion valinta</h1>
+            </div>
+        )
+    }
+}
+
+export default componentName
